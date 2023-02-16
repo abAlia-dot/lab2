12cb7d3096a5bb30b9c918563c07666c20f05cf8
abalia <abalia@student.apc.edu.ph>
2023-02-16 12:36:36 +0800

    Updated Forms 2

--- Week 9/guest.php
+++ Week 9/guest.php
@@ -4,10 +4,10 @@
 <body>
 
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mydata";
+$servername = "192.168.150.213";
+$username = "webprogss211";
+$password = "fancyR!ce36";
+$dbname = "webprogss211";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -16,13 +16,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$sql = "SELECT id, name, email, website, comment, gender, reg_date FROM abalia_myguest";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
+        echo "<br> id: ". $row["id"]. " - Name: ". $row["name"]. " - Email: " . $row["email"] . "<br>";
     }
 } else {
     echo "0 results";
