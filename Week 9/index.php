3867e6c953cad24052a611b89854d867c54e5a53
abalia <abalia@student.apc.edu.ph>
2023-02-16 12:46:00 +0800

    Update 4

--- Week 9/index.php
+++ Week 9/index.php
@@ -332,34 +332,7 @@ echo "<br>";
 echo $gender;
 ?>
 
-<?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
-
-    $servername = "192.168.150.213";
-    $username = "webprogss211";
-    $password = "fancyR!ce36";
-    $dbname = "webprogss211";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
-
-    $sql = "INSERT INTO abalia_myguest (name, email, website, comment, gender)
-  VALUES ('$name','$email','$website','$comment','$gender')";
-    if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-    } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
-    $conn->close();
-}
-?>
 
 
 
@@ -398,6 +371,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
 
+  <?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+
+    $servername = "192.168.150.213";
+    $username = "webprogss211";
+    $password = "fancyR!ce36";
+    $dbname = "webprogss211";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO abalia_myguest (name, email, website, comment, gender)
+  VALUES ('$name','$email','$website','$comment','$gender')";
+    if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
  
     
 </body>
